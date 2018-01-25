11e437bfb0375f15cb637782f8beba87f9cf1f2e
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2018-01-25 18:43:33 +0100

    Fixing CMake

--- hexapod_driver/src/hexapod_driver_cartesian.cpp
+++ hexapod_driver/src/hexapod_driver_cartesian.cpp
@@ -1,12 +1,12 @@
-#include <hexapod_driver/hexapod.hpp>
+#include <hexapod_driver/hexapod_cartesian.hpp>
 #include <ros/ros.h>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "hexapod_driver");
+    ros::init(argc, argv, "hexapod_driver_cartesian");
     ros::NodeHandle n;
 
-    auto hexa = std::make_shared<hexapod_ros::Hexapod>(n);
+    auto hexa = std::make_shared<hexapod_ros::HexapodCartesian>(n);
     hexa->move({1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0.5, 0.5, 0.25, 0.75, 0.5, 1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5, 1, 0.5, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5}, 5.0);
     auto t = hexa->transform();
     auto p = t.getOrigin();
